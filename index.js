f19c55b8adcc6b7ab94e47b11efa076d0afca185
vitalii-gh25 <a.g.vitalii2022@gmail.com>
2025-10-03 18:45:57 +0000

    Deploying to gh-pages from @ vitalii-gh25/goit-js-hw-11@591c085ae0b98fa0e8f03be5dee298846ca1f0b3 🚀

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import{a as p,S as m,i as a}from"./assets/vendor-D8hBcPQM.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const y="52586947-52be226ea0996a0c8fac5fdee",g="https://pixabay.com/api/";function h(s){return p.get(g,{params:{key:y,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(t=>t.data).catch(t=>(console.error("AxiosError:",t),{hits:[]}))}const l=document.querySelector(".gallery"),u=document.getElementById("loader");let b=new m(".gallery a",{captionsData:"alt",captionDelay:250});function v(s){const t=s.map(({webformatURL:o,largeImageURL:i,tags:e,likes:r,views:n,comments:d,downloads:f})=>`
       <li class="gallery-item">
-      <div class='gallery-img>
+      <div class='gallery-img'>
           <a href="${i}">
             <img src="${o}" alt="${e}" loading="lazy" />
           </a>
